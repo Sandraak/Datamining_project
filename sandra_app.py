65a923ef44944a95367d33dc7793e80e01ef6c7a
Sandra ter Maat <HS.terMaat@student.han.nl>
2022-12-02 13:44:26 +0100

    predict and accuracy

--- sandra_app.py
+++ sandra_app.py
@@ -24,10 +24,27 @@ class Pegasos:
                 self.w = (1 - learning_rate*self.lambda1)*self.w + learning_rate*self.y*self.x
             else:
                 self.w = (1- learning_rate*self.lambda1)*self.w
-        print("fit")
 
-    def predict(data) -> np.array:
-        print("predict")
+    def predict(self,data) -> np.array:
+        predicted_labels:np.array
+        xi:np.array
+        for i in range(len(data)):
+                xi = data[i]
+                dot_product:float = 0.0
+                for j in range(len(xi)):
+                    dot_product += self.w[j]*xi[j]
+                if(dot_product >= 0):
+                    predicted_labels.append(1)
+                else:
+                    predicted_labels.append(-1)
+        return predicted_labels
 
-    def accuracy(label, predicted_label) -> float:
-        print("accuracy")
+
+    def accuracy(labels:np.array, predicted_labels:np.array) -> float:
+        correct_pred:int = 0
+        for i in range(len(predicted_labels)):
+            if labels[i] == predicted_labels[i]:
+                correct_pred += 1
+
+        return float(correct_pred/len(labels))
+
