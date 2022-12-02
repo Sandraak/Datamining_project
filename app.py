0dad8fc27e3bd0996bebd08e1e0abf70251edb26
JannekeNouwen <j.nouwen@student.han.nl>
2022-12-02 15:07:01 +0100

    het werkt, 0.6 accuracy ong

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
 from pegasos import Pegasos
 
 
 def main():
-	X, y = prepare_data("codon_usage.csv")
-	model = Pegasos(epochs=10, lambda1=1)
-	model.fit(X, y)
-
+	X_train, X_test, y_train, y_test = prepare_data("codon_usage.csv")
+	model = Pegasos(n_iter=len(X_train)*100, lambda1=1)
+	model.fit(X_train, y_train)
+	y_pred = model.predict(X_test)
+	print(accuracy_score(y_test, y_pred))
 
 def prepare_data(filename):
 	data = pd.read_csv(filename, low_memory=False)
@@ -17,7 +21,9 @@ def prepare_data(filename):
 	X = data.iloc[:,-64:].to_numpy()
 	y = data.iloc[:, 0].to_numpy()
 
-	return X, y
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+	return X_train, X_test, y_train, y_test
 
 
 main()
--- pegasos.py
+++ pegasos.py
@@ -1,11 +1,10 @@
 import numpy as np
-from sklearn.svm import SVC
 import matplotlib.pyplot as plt
 
 
 class Pegasos:
-	def __init__(self, epochs:int = 10, lambda1:int = 1) -> None:
-		self.epochs: int = epochs
+	def __init__(self, n_iter:int = 10, lambda1:int = 1) -> None:
+		self.n_iter: int = n_iter
 		self.lambda1: int = lambda1
 		self.X: np.ndarray
 		self.y: np.ndarray
@@ -31,14 +30,13 @@ class Pegasos:
 		# Initialize the weight vector for the perceptron with zeros
 		self.w = np.zeros(n_features)
 
-		for epoch in range(self.epochs):
+		for i in range(self.n_iter):
 			error = 0
-			learning_rate = 1. / (self.lambda1*(epoch+1))
+			learning_rate = 1. / (self.lambda1*(i+1))
 			rand_sample_index = np.random.choice(n_samples, 1)[0]
 			sample_X, sample_y = self.X[rand_sample_index], self.classes[rand_sample_index]
 			score = self.w.dot(sample_X)
 
-			print(sample_y)
 			if sample_y*score < 1:
 				self.w = (1 - learning_rate*self.lambda1)*self.w + learning_rate*sample_y*sample_X
 				error = 1
@@ -46,8 +44,6 @@ class Pegasos:
 				self.w = (1 - learning_rate*self.lambda1)*self.w
 
 			errors.append(error)
-
-		print(self.w)
 
 	def find_classes(self):
 		class_num = -1
@@ -59,13 +55,25 @@ class Pegasos:
 				class_num += 2
 			self.classes[i] = self.class_names_nums[label]
 
-	def plot_errors(self, errors):
-		# plt.plot(errors, '|')
-		# plt.ylim(0.5,1.5)
-		# plt.axes().set_yticklabels([])
-		# plt.xlabel('Epoch')
-		# plt.ylabel('Misclassified')
-		# plt.show()
-		pass
+	def predict(self, X:np.ndarray):
+		predicted_labels = []
+		for sample in X:
+			dot_product = 0
+			for i in range(len(sample)):
+				dot_product += self.w[i] * sample[i]
+			if dot_product >= 0:
+				predicted_labels.append(1)
+			else:
+				predicted_labels.append(-1)
+
+		return [self.class_nums_names[label_num] for label_num in predicted_labels]
+	# def plot_errors(self, errors):
+	# 	plt.bar(errors)
+	# 	# plt.ylim(0.5,1.5)
+	# 	# plt.axes().set_yticklabels([])
+	# 	# plt.xlabel('Epoch')
+	# 	# plt.ylabel('Misclassified')
+	# 	plt.show()
+		
 
 # Pegasos(epochs=20, lambda1=3)
